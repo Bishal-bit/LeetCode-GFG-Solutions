9d32a069112daba026ca307d615931c03bc83f01
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-07-31 13:22:54 +0530

    Time: 0 ms (100%), Space: 19.9 MB (81.37%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        low, high=0, n-1
+        #Initialize ans as n in case no element is greater than target
+        ans=n
+        while low<=high :
+            #Calculate mid
+            mid=low + (high-low)//2
+            #If nums[mid]>=target then update ans as mid and check the left portion by high=mid-1
+            if nums[mid]>=target :
+                ans=mid
+                high=mid-1
+            #Else update low as mid+1
+            else : 
+                low=mid+1
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
