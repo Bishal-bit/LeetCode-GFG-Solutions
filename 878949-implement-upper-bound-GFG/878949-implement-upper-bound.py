9df9e26ac13e731ddd0ad6d95cb1173894461238
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-07-31 13:12:55 +0530

    Added solution - Time: O(log n), Space: O(1)

---
+++ 878949-implement-upper-bound-GFG/878949-implement-upper-bound.py
@@ -0,0 +1,22 @@
+class Solution:
+    def upperBound(self, arr, target):
+        # code here
+        n=len(arr)
+        low, high=0, n-1
+        #Initialize ans as n in case no element is greater than target
+        ans=n
+        while low<=high :
+            #Calculate mid
+            mid=low + (high-low)//2
+            #If arr[mid]>target then update ans as mid and check the left portion by high=mid-1
+            if arr[mid]>target :
+                ans=mid
+                high=mid-1
+            #Else update low as mid+1
+            else : 
+                low=mid+1
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
