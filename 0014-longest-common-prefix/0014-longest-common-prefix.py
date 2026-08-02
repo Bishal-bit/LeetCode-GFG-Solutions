1a257a4e068d0e091346868a7829206ceb2d22a9
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-02 12:27:40 +0530

    Time: 0 ms (100%), Space: 19.4 MB (30.84%) - LeetHub - Time: O(n * m), Space: O(1)

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        #If list is empty then return empty string
+        if not strs : return ""
+        #Pre= 1st string of list
+        pre=strs[0]
+        n=len(pre)
+        #Traverse through characters of pre i.e. 1st string
+        for i in range(n) :
+            #Strore character to ch
+            ch=pre[i]
+            #Traverse through other strings of list
+            #Match character by character
+            #If mismatch happens then return substring
+            for word in strs[1:] :
+                if i==len(word) or ch!=word[i] : return pre[:i]
+        #If no mismatch then return whole string of pre
+        return pre
+
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
