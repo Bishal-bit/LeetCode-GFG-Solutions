2d323a766eae599e23eb695b11856959564fb4d8
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-02 12:37:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (15.5%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        ns, ng=len(s), len(goal)
+        #Length is different then return False
+        if ns!=ng : return False
+        #s+=s
+        s+=s
+        #Check if goal is a part of s or not
+        if s.find(goal)!=-1 : return True
+        return False
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
