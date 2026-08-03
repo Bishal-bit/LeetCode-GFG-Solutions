e6305a95a9d3db12d7faece96fa09d3c4090850a
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-03 15:22:58 +0530

    Added solution - Time: O(N), Space: O(1)

---
+++ 700039-length-of-linked-list-GFG/700039-length-of-linked-list.py
@@ -0,0 +1,20 @@
+''' Structure of linked list Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def getCount(self, head):
+        # code here
+        count=0
+        temp=head
+        while temp :
+            #Increment count for each node of linked list
+            count+=1
+            temp=temp.next
+        return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
