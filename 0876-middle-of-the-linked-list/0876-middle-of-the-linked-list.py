2ceac2979781a9bb8e385fb3dc5a7bb66e1beaf8
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-03 16:20:16 +0530

    Time: 1 ms (1.96%), Space: 19.4 MB (19.02%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Declare 2 nodes as slow, fast
+        slow=fast=head
+        #Condition must be on fast as we will adjust mid pision based on this condition
+        while fast and fast.next :
+            #Slow single step, Fast double step
+            slow=slow.next
+            fast=fast.next.next
+        #Slow is at mid position
+        return slow
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
