14e4bcc9e16a3909f9269e2ff3f34a0dd7f44177
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-03 15:19:41 +0530

    Time: 45 ms (73.28%), Space: 19.5 MB (65.34%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        #node is the node on which arrow is pointed in Question. We have to remove it.
+        #node.next.val is copied to the node.val
+        node.val=node.next.val
+        #node.next is connected to node.next.next to skip a node
+        node.next=node.next.next
+
+
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
