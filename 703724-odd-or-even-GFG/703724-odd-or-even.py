4b61fa67270d0dc668b36b0dfe9a52a0fbde1687
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-06 15:08:43 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isEven (self, n):
+        # code here 
+        if n & 1 : return False
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
