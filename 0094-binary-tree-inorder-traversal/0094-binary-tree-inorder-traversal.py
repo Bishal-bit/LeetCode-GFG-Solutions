ca82203985b90842f38c83d4fffd0946ed61aede
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-08-08 14:51:38 +0530

    Time: 0 ms (100%), Space: 19.1 MB (92.29%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfs(self, root: Optional[TreeNode], ans :List[int]) :
+        #If root==NULL then just return
+        if not root : return
+        #left->root->right
+        self.dfs(root.left,ans)
+        ans.append(root.val)
+        self.dfs(root.right,ans)
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        #DFS Traversal
+        self.dfs(root, ans)
+        #Return ans
+        return ans
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
