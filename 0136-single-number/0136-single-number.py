1984c46cf0835f862e52aa43377a95977c7541d0
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 12:16:26 +0530

    Time: 3 ms (60.44%), Space: 20.9 MB (93.74%) - LeetHub - Time: O(n), Space: O(1)

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        #Initialize exor1 as 0
         exor1=0
         for it in nums :
-            #Do exor1 operation with every elements of list
             exor1^=it
         return exor1
-        
+
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
