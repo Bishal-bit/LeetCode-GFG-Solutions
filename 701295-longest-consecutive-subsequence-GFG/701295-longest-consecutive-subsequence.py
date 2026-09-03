910678b2589991583aafc0ffa2bcd942bf7caa8a
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 19:53:27 +0530

    Added solution - Time: O(n), Space: O(n)

--- 701295-longest-consecutive-subsequence-GFG/701295-longest-consecutive-subsequence.py
+++ 701295-longest-consecutive-subsequence-GFG/701295-longest-consecutive-subsequence.py
@@ -2,22 +2,25 @@ class Solution:
     def longestConsecutive(self, arr):
         # code here
         #Pass whole list to a set
-        s=set(arr)
+        st=set(arr)
         #Initialize maxi as 0
         maxi=0
-        #Traverse for each and every element
         for it in arr :
-            target=it   #Store target=it 
-            #If target-1 not in set then it might be the start of a consequtive sequence
-            if target-1 not in s:
-                count=0     #Store count as 0
-                while target in s:
-                    #If target found then increment count,update maxi, increment target
-                    count+=1 
-                    maxi=max(maxi,count)
-                    target+=1
+            #If it-1 in set then continue
+            if it-1 in st : continue
+            target=it       #Store target=it 
+            count=0         #Store count as 0
+            while target in st : 
+                #If target found then increment count,increment target
+                count+=1
+                target+=1
+            #Update maxi
+            maxi=max(maxi, count)
         #Return maxi
-        return maxi            
+        return maxi
+        
+        
+        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
