8372574e4b55b3a8fb17535e46c8d0d436398c22
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 15:50:32 +0530

    Time: 0 ms (100%), Space: 20.5 MB (41.6%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,13 +3,15 @@ class Solution:
         #Declare a map
         mp={}
         #Treverse through the array
-        for i, it in enumerate(nums) : 
-            #Rem= difference b/w current element it and our target
-            rem=target-it
-            #If rem is there in map then return it's index, rem's index
-            if rem in mp : return [i, mp[rem]]
+        for i, it in enumerate(nums) :
+            #Remain= difference b/w current element it and our target
+            remain=target-it
+            #If remain is there in map then return it's index, rem's index
+            if remain in mp : return [i, mp[remain]]
             #Strore mp[it]=i
             mp[it]=i
+        
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
