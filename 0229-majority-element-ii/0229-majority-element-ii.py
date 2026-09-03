fe6e03f1afc2fa902a4b9c507c81b1e83b9dc2d9
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 22:54:50 +0530

    Time: 4 ms (80.85%), Space: 23.6 MB (43.72%) - LeetHub - Time: O(n), Space: O(n)

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,19 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n=len(nums)
-        #Declare a map and a list
+        #Declare a map 
         freq={}
-        ans=[]
         #Traverse through the list pass elements to freq based on frequency
         for it in nums :
             if it in freq : freq[it]+=1
             else : freq[it]=1
+        ans=[]
         #Store keys, having value > n/3
         for key, value in freq.items() :
-            if value>n/3 : ans.append(key)
+            if value > n/3 : ans.append(key)
         return ans
-
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
