704f20078b261c8fd0d33ac39f8dde5c1fd6d9d1
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 23:10:05 +0530

    Time: 622 ms (64.27%), Space: 22.1 MB (82.3%) - LeetHub - Time: O(n^2), Space: O(n)

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -10,23 +10,22 @@ class Solution:
             if i>0 and nums[i]==nums[i-1] : continue
             j, k=i+1, n-1
             while j<k :
-                total=nums[i]+nums[j]+nums[k]
+                total=nums[i] + nums[j] + nums[k]
                 #total>0 then decrease k
                 if total>0 : k-=1
                 #total<0 then increase j
                 elif total<0 : j+=1
                 #If total==0 then push increase j, decrease k
-                else :
-                    ans.append([nums[i],nums[j],nums[k]])
+                else : 
+                    ans.append([nums[i], nums[j], nums[k]])
                     j+=1
                     k-=1
                     #Here we are skipping duplicates 
                     #nums[j],nums[k] can not contain same value for different triplets
-                    while(j<k and nums[j]==nums[j-1]) : j+=1
-                    while(j<k and nums[k]==nums[k+1]) : k-=1
+                    while j<k and nums[j]==nums[j-1] : j+=1
+                    while j<k and nums[k]==nums[k+1] : k-=1
         return ans
 
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
