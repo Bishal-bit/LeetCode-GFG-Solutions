788e259f7b3615640df4dce01b8f3969e20c6b0f
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-03 16:03:25 +0530

    Time: 14 ms (21.02%), Space: 20.9 MB (98.15%) - LeetHub - Time: O(n), Space: O(1)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,26 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #Initialize element as None and count as 0
-        element=None
+        #Initialize el as None and count as 0
+        el=None
         count=0
         for it in nums :
-            #If count==0 reinitialize element as it and set count=1
+            #If count==0 reinitialize el as it and set count=1
             if count==0 :
-                element=it
+                el=it
                 count=1
-            #If it==element then increment count
-            elif it==element : count+=1
-            #If it!=element then decrement count
-            elif it!=element : count-=1
+            #If el==it then increment count
+            elif el==it : count+=1
+            #If el!=it then decrement count
+            elif el!=it : count-=1
         #Set count=0
         count=0
         for it in nums :
-            if it==element : count+=1
+            if it==el : count+=1
         n=len(nums)
-        #If count exceeds n/2 then return element
-        if count>n/2 : return element
+        #If count exceeds n/2 then return element else -1
+        return el if count>n/2 else -1
 
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
