8739665523f6f203527708cbffadc6f6e7a68a40
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 12:10:31 +0530

    Added solution - Time: O(log n), Space: O(1)

--- 703129-number-of-occurrence-GFG/703129-number-of-occurrence.py
+++ 703129-number-of-occurrence-GFG/703129-number-of-occurrence.py
@@ -1,38 +1,39 @@
 class Solution:
-    def first1(self, arr, target, n) -> int :
-        first=-1
+    def first1(self, arr, target) :
+        n=len(arr)
         left, right=0, n-1
-        while left<=right :
+        ans=-1
+        while left<= right :
             mid=left+ (right-left)//2
             if arr[mid]==target :
-                #Update first and check for left side portion by updating right
-                first=mid
+                #Update ans and check for left side portion by updating right
+                ans=mid
                 right=mid-1
             elif arr[mid]<target : left=mid+1
             else : right=mid-1
-        return first
+        return ans
 
-    def last1(self, arr, target, n) -> int :
-        last=-1
+    def last1(self, arr, target) :
+        n=len(arr)
         left, right=0, n-1
-        while left<=right :
+        ans=-1
+        while left<= right :
             mid=left+ (right-left)//2
             if arr[mid]==target :
-                #Update last and check for right side portion by updating left
-                last=mid
+                #Update ans and check for right side portion by updating left
+                ans=mid
                 left=mid+1
             elif arr[mid]<target : left=mid+1
             else : right=mid-1
-        return last
+        return ans
+        
     def countFreq(self, arr, target):
         # code here
-        n=len(arr)
-        a=self.first1(arr,target,n)
+        a=self.first1(arr, target)
         #If first index is not found then no need to find the second index
         if a==-1 : return 0
-        b=self.last1(arr,target,n)
-        return b-a+1
-        
+        b=self.last1(arr, target)
+        return b-a+1    #a...b
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
