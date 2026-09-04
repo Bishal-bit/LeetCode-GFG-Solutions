32178ffc5d87c9a0d8d9a0c275c922ae0088e4b3
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 11:21:45 +0530

    Added solution

--- 701222-floor-in-a-sorted-array-GFG/701222-floor-in-a-sorted-array.py
+++ 701222-floor-in-a-sorted-array-GFG/701222-floor-in-a-sorted-array.py
@@ -5,15 +5,15 @@ class Solution:
         low, high=0, n-1
         #Initialize ans as n in case no element is greater than target
         ans=-1
-        while low<=high :
+        while low<= high :
             #Calculate mid
-            mid=low + (high-low)//2
+            mid=low+ (high-low)//2
             #If arr[mid]>=x then update ans as mid and check the left portion by high=mid-1
             if arr[mid]<=x :
                 ans=mid
                 low=mid+1
             #Else update low as mid+1
-            else : 
+            else :
                 high=mid-1
         return ans
 
