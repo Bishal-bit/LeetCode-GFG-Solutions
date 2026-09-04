ab32c83e6ca1bff76a363e98c3113c75170f9ab5
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 22:27:52 +0530

    Time: 15 ms (43.54%), Space: 19.4 MB (46.2%) - LeetHub - Time: O(n), Space: O(1)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         ns, nt=len(s), len(t)
+
         #If strings having different length then return False
         if ns!=nt : return False
+
         #Declare list as we will deal based on ASCII values
         freq=[0]*26
         for i in range(ns) :
             #ord() is for ASCII values of characters
             freq[ord(s[i])-ord('a')]+=1
             freq[ord(t[i])-ord('a')]-=1
+        
         #If any element of freq is not 0 then return False
-        for it in freq : 
+        for it in freq :
             if it!=0 : return False
+        
         #Return True
         return True
 
