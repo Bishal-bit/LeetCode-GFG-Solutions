e0bd9b173a78c57b8e31405efe194e80134345f6
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 11:23:51 +0530

    Added solution

--- 889109-ceil-in-a-sorted-array-GFG/889109-ceil-in-a-sorted-array.py
+++ 889109-ceil-in-a-sorted-array-GFG/889109-ceil-in-a-sorted-array.py
@@ -3,17 +3,17 @@ class Solution:
         # code here
         n=len(arr)
         low, high=0, n-1
-        #Initialize ans as n in case no element is greater than target
+        #Initialize ans as -1 in case no element is greater than target
         ans=-1
-        while low<=high :
+        while low<= high :
             #Calculate mid
-            mid=low + (high-low)//2
+            mid=low+ (high-low)//2
             #If arr[mid]>=x then update ans as mid and check the left portion by high=mid-1
             if arr[mid]>=x :
                 ans=mid
                 high=mid-1
             #Else update low as mid+1
-            else : 
+            else :
                 low=mid+1
         return ans
 
