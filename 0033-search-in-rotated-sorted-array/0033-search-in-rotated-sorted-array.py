567302330cd7ffcfad9b6c04fbd2b868eea430a4
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 14:58:17 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.49%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -5,19 +5,22 @@ class Solution:
         while left<=right :
             mid=left+ (right-left)//2
             #If target is in mid index then return mid
-            if nums[mid]==target : return mid
+            if nums[mid]== target : return mid
+            
             #If left---mid portion is sorted
             elif nums[left]<=nums[mid] :
                 #If target in in b/w left and mid then shrink the right side
                 if nums[left]<=target<nums[mid] : right=mid-1
                 #Else shrink left side
                 else : left=mid+1
-            ##If mid---right portion is sorted
-            else : 
+            
+            #If mid---right portion is sorted
+            else :
                 if nums[mid]<target<=nums[right] : left=mid+1
                 else : right=mid-1
-        #Return -1
+        #return -1
         return -1
+            
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
