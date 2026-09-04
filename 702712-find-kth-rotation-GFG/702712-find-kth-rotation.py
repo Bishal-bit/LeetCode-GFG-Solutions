fb3355ff7fafcc66c11617c9401a95baf4479415
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 15:43:39 +0530

    Added solution - Time: O(log n), Space: O(1)

--- 702712-find-kth-rotation-GFG/702712-find-kth-rotation.py
+++ 702712-find-kth-rotation-GFG/702712-find-kth-rotation.py
@@ -2,36 +2,38 @@ class Solution:
     def findKRotation(self, arr):
         # code here
         n=len(arr)
+        mini=float('inf')
+        index=-1
         left, right=0, n-1
-        #Initialize ans as -inf and index as None
-        ans=float('inf')
-        index=None
-        while left<=right :
+        while left<= right :
             mid=left+ (right-left)//2
             #If whole list is sorted left---mid---right
-            #Update ans and index then break
-            if arr[left]<arr[right] :
-                if arr[left]<ans :
-                    ans=arr[left]
+            #If arr[left]< mini, update mini and break
+            if arr[left]<= arr[right] : 
+                if arr[left]< mini :
+                    mini=arr[left]
                     index=left
                 break
+        
             #if left---mid portion is sorted
-            #Update ans and index then check for right portion by updating left
-            elif arr[left]<=arr[mid] :
-                if arr[left]<ans :
-                    ans=arr[left]
+            #if arr[left]< mini, update mini  
+            #check for right portion by updating left
+            elif arr[left]<= arr[mid] :
+                if arr[left]< mini :
+                    mini=arr[left]
                     index=left
                 left=mid+1
+        
             #if mid---right portion is sorted
-            #Update ans and index then check for left portion by updating right
-            else :
-                if arr[mid]<ans :
-                    ans=arr[mid]
+            #if arr[mid]<mini, update mini 
+            #check for left portion by updating right
+            else:
+                if arr[mid]<mini :
+                    mini=arr[mid]
                     index=mid
                 right=mid-1
         #Return index
-        return index
-                
+        return index   
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
