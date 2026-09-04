b60d7bdcbbe2b4aad52aa95caaef2c430bc9f00b
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 19:44:55 +0530

    Time: 3 ms (95.14%), Space: 20.2 MB (64.64%) - LeetHub - Time: O(n), Space: O(1)

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,17 +1,17 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        #If already a odd no then return it
-        if int(num[-1])%2==1 : return num
         n=len(num)
+        #If already an odd no then return it
+        if int(num[-1])%2==1 : return num
+        
         #Finding index of last odd no
-        for i in range(n-1,-1,-1) :
+        for i in range(n-1, -1, -1):
             if int(num[i])%2==1 : break
         #0--0. Only one digit that is 0th digit. If it is even then return empty string
         if i==0 and int(num[i])%2==0 : return ""
+
         #Return substring 0--i
         return num[:i+1]
-        
-        
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
