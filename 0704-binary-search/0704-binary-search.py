22f99e8f7e6c86f930b83ea806e54a787354c5d6
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 10:37:59 +0530

    Time: 0 ms (100%), Space: 20.5 MB (37.22%) - LeetHub - Time: O(log n), Space: O(1)

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,10 +3,10 @@ class Solution:
         #Binary Search
         n=len(nums)
         low, high=0, n-1
-        while(low<=high) :
+        while low<=high :
             mid=low+ (high-low)//2
             if nums[mid]==target : return mid
-            elif nums[mid]<target : low=mid+1
+            elif nums[mid]< target : low=mid+1
             else : high=mid-1
         #Return -1
         return -1
