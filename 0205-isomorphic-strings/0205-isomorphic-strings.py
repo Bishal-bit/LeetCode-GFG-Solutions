4d4574dae45eb1bbebc6a39d70ae2d3aae06b247
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-04 21:56:06 +0530

    Time: 7 ms (38.54%), Space: 19.1 MB (83.18%) - LeetHub - Time: O(n), Space: O(n)

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,24 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        #Declare 2 maps
-        mapst={}
-        mapts={}
-        #Iterate through both
-        for i,j in zip(s,t) :
-            #If element is in map then check if there is any mismatch
-            if i in mapst :
-                if mapst[i]!=j : return False
-            #Else add it in map
-            else : mapst[i]=j
-            #If element is in map then check if there is any mismatch
-            if j in mapts :
-                if mapts[j]!=i : return False
-            #Else add it in map
-            else : mapts[j]=i
-        #No mismatch. So return True
-        return True
-                
+        s_to_t={}
+        t_to_s={}
+        #Iterate through both by using zip()
+        for a,b in zip(s, t) :
+            #If a is in map s_to_t and any mismatch happens return False
+            #If b is in map t_to_s and any mismatch happens return False
+            if a in s_to_t and s_to_t[a]!=b : return False
+            if b in t_to_s and t_to_s[b]!=a : return False
 
+            #Store a, b to maps s_to_t and t_to_s
+            s_to_t[a]=b
+            t_to_s[b]=a
+        
+        return True
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
