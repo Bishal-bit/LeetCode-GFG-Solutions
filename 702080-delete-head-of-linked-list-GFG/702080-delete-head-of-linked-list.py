060445f3e5d9d8e69f316b821056dddac50d2f83
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-05 11:07:32 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 702080-delete-head-of-linked-list-GFG/702080-delete-head-of-linked-list.py
@@ -0,0 +1,16 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def deleteHead(self, head):
+        # code here
+        return head.next
+    
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
