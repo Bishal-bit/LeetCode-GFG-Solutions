798080c075c9ed5f73ec7bd2d03ff647c89ebbb4
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-05 22:36:32 +0530

    Time: 188 ms (26.48%), Space: 40.8 MB (31.85%) - LeetHub - Time: O(n log n), Space: O(log n)

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,10 +5,12 @@
 #         self.next = next
 class Solution:
     def merge(self, ll1: Optional[ListNode], ll2: Optional[ListNode]) :
+        #Use dummy node for creating new LL
+        dummy=dummyhead=ListNode(-1)
+        
         temp1, temp2=ll1, ll2
-        dummy=dummyhead=ListNode(0)
         while temp1 and temp2 :
-            if temp1.val<=temp2.val :
+            if temp1.val <= temp2.val :
                 dummy.next=temp1
                 temp1=temp1.next
             else :
@@ -16,23 +18,25 @@ class Solution:
                 temp2=temp2.next
             dummy=dummy.next
         dummy.next=temp1 if temp1 else temp2
+        #Return dummyhead.next
         return dummyhead.next
-    
+
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #Find middle of LL create 2 LL head---mid and mid+1---None
         if not head or not head.next : return head
-        slow, fast=head, head.next
+        #Find middle of LL  to create 2 LL head---mid and mid+1---None
+        #head---temp     slow---None
+        slow=fast=head
         while fast and fast.next :
+            temp=slow
             slow=slow.next
             fast=fast.next.next
-        temp=slow.next
-        slow.next=None
+        #Break the connection by temp.next=None
+        temp.next=None
+        
         ll1=self.sortList(head)
-        ll2=self.sortList(temp)
+        ll2=self.sortList(slow)
         #Do merge operation to sort the LL
         return self.merge(ll1,ll2)
-
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
