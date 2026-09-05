1158b32d00d39a6ead939fee3c24246b83477e72
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-05 15:42:36 +0530

    Time: 56 ms (42.75%), Space: 22.4 MB (97.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -8,15 +8,17 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         #Declare 2 nodes as slow, fast
         slow=fast=head
+        
         #Condition must be on fast as it's movement is more
         while fast and fast.next :
             #Slow single step, Fast double step
             slow=slow.next
             fast=fast.next.next
-            #If slow==fast then loop exists.
+            #If slow==fast then loop exists
             if slow==fast : return True
-        return False
         
+        #Return slow
+        return False
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
