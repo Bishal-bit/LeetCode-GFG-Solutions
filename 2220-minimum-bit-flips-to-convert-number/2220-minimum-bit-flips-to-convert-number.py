78238e5ea630fa45a926a54f009918fda71da3eb
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 22:38:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (26.68%) - LeetHub - Time: O(log n), Space: O(1)

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,11 +1,15 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
+        #Change is the change of bits between start and goal
         change=start ^ goal
         count=0
+        #Count set bits of change
         while change :
-            if change & 1 ==1 : count+=1
+            if change & 1 : count+=1
             change>>=1
+        #Return count
         return count
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
