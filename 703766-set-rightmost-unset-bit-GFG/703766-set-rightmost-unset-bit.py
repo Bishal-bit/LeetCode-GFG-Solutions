e2fc28355e343b9e920d4b1684254d37987f9fe2
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 20:07:47 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703766-set-rightmost-unset-bit-GFG/703766-set-rightmost-unset-bit.py
@@ -0,0 +1,11 @@
+class Solution:
+    def setBit(self, n):
+        # code here
+        #if we go from n to (n+1) the right most unset bit of n is to be changed
+        #  011 | 100=>111
+        # 1010 | 1011=>1011
+        return n | (n+1)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
