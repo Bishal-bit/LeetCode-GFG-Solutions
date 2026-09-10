e34195fa2bb13e7b01a6ae0733111b21b3014206
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 11:13:16 +0530

    Time: 3 ms (94.99%), Space: 19.3 MB (91.79%) - LeetHub - Time: O(2^t), Space: O(t)

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,20 @@
 class Solution:
-    def func(self,index :int,candidates: List[int], target: int,n: int,v : List[int],ans :List[List[int]]) :
+    def func(self, index :int, candidates: List[int], target: int, n :int, v:List[int], ans :List[List[int]]) :
         #Use index, target if condition separately to get rid of index out of bound error
         if index==n :
-            if target==0 : 
-                #We want stored ans to be unchanged that is why used v.copy()
-                ans.append(v.copy())
+            if target==0 : ans.append(v.copy())
             return
         
         if candidates[index]<=target :
+            #We want the stored ans to be unchanged that is why used v.copy()
             v.append(candidates[index])
             self.func(index, candidates, target-candidates[index], n, v, ans)
-            v.pop()     #v.pop() for backtracking
+            v.pop()         #v.pop() for backtracking
+        
         #Go for next index
         self.func(index+1, candidates, target, n, v, ans)
 
+        
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         n=len(candidates)
         ans=[]
