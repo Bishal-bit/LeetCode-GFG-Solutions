f8ab3dcf51f3caa85b1f7de8b23170c0c91c93de
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 11:13:25 +0530

    Time: 11 ms (55.99%), Space: 19.4 MB (73.92%) - LeetHub - Time: O(2^n * n), Space: O(n)

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,12 +1,14 @@
 class Solution:
-    def func(self,index :int,candidates: List[int], target: int,n: int,v : List[int],ans :List[List[int]]) :
+    def func(self, index: int, candidates: List[int], target: int, n: int, v: List[int], ans: List[List[int]]) :
         if target==0 :
             ans.append(v.copy())
             return
+        
         #Use for loop
-        for i in range(index,n) :
+        for i in range(index, n) :
             #If same numerical no is repeated then skip them
             if i>index and candidates[i]==candidates[i-1] : continue
+            
             if candidates[i]<=target :
                 v.append(candidates[i])
                 self.func(i+1, candidates, target-candidates[i], n, v, ans)
