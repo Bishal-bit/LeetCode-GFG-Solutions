6b01993fb1ad930cd27105c8e96d01a57af1bfc0
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 19:52:29 +0530

    Added solution - Time: O(1), Space: O(1)

--- 701154-check-kth-bit-GFG/701154-check-kth-bit.py
+++ 701154-check-kth-bit-GFG/701154-check-kth-bit.py
@@ -1,9 +1,7 @@
 class Solution:
     def checkKthBit(self, n, k):
         # code here
-        #kth digit is 1, rest of the right side digits are 0
-        val=1<<k
-        return (n & val)
+        return n>>k & 1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
