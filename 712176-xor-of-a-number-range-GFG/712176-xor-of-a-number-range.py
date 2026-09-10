a3d0e28eada09f0f4aa43dd88839565b4b3bedf7
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 23:12:37 +0530

    Added solution - Time: O(1), Space: O(1)

--- 712176-xor-of-a-number-range-GFG/712176-xor-of-a-number-range.py
+++ 712176-xor-of-a-number-range-GFG/712176-xor-of-a-number-range.py
@@ -1,12 +1,17 @@
 class Solution:
-    def func(self,n) :
-        if n%4==0 : return n
-        elif n%4==1 : return 1
-        elif n%4==2 : return n+1
+    def func(self, x: int) :
+        if x%4==0 : return x
+        elif x%4==1 : return 1
+        elif x%4==2 : return x+1
         else : return 0
+        
     def findXOR(self, l, r):
         # code here
-        return self.func(l-1) ^ self.func(r)
+        #0----l----r
+        #We want exor of l----r
+        # exor of 0---l-1 exor of 0---r => exor of l---r
+        return self.func(l-1) ^ self.func(r) 
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
