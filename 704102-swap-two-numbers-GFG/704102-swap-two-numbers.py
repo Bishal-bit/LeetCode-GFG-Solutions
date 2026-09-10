a0553ef5274124cb8cc1f58ebaa4cd0c052bd22b
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-10 20:09:06 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704102-swap-two-numbers-GFG/704102-swap-two-numbers.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+# code here
+a=a^b
+b=a^b
+a=a^b
+print(a, b)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
