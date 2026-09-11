85379501066d2ea7a6a99f93607345a685660d01
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-11 20:20:20 +0530

    Time: 23 ms (40.99%), Space: 30.9 MB (40.99%) - LeetHub - Time: O(k), Space: O(1)

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,26 +1,25 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         n=len(cardPoints)
-        #Initialize sum, maxi as 0, 0
-        sum, maxi=0, 0
+        sum=0       #Initialize sum as 0
         #Count sum for 0th to (k-1)th element
-        for i in range(k) : sum+=cardPoints[i]
-        #Update maxi as sum
-        maxi=sum
+        for i in range(k) : 
+            sum+=cardPoints[i]
+        #Initialize ans as sum
+        ans=sum
         for i in range(k) :
             #Remove one element from (right most)left
             sum-=cardPoints[k-1-i]
             #Adding one element from (right most)right
             sum+=cardPoints[n-1-i]
-            #Update Maxi
-            maxi=max(maxi, sum)
+            #Update ans
+            ans=max(ans, sum)
             """
                  left:2 right:6
             then left:1 right:5
             then left:0 right:4
             """
-        return maxi
-
+        return ans
 
 
 # Synced seamlessly with LeetHub Pro
