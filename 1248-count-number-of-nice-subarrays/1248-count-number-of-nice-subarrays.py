e9d9bbe118113978bbe57562b4a1b923f0f21d45
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-11 18:17:01 +0530

    Time: 166 ms (9.25%), Space: 24.6 MB (91.96%) - LeetHub - Time: O(n), Space: O(1)

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,28 +1,28 @@
 class Solution:
-    def func(self, nums: List[int], k: int) -> int:
+    def func(self, nums: List[int], k: int) :
+        if k<0 : return 0
         n=len(nums)
-        ans=0
-        left, right=0, 0
+        left=right=0
+        count=0
         while right<n :
             #If odd then decrement k
-            if nums[right]%2==1: 
-                k-=1     
+            if nums[right]%2==1 : k-=1
             #While k<0 : 
             #           if nums[left] is odd then increment k
             #           increment left
-            while k<0:
-                if nums[left]%2==1: k+=1
+            while k<0 :
+                if nums[left]%2==1 : k+=1
                 left+=1
-            #Update ans, increment right
-            ans+=right-left+1
+            #Update count, increment count
+            count+=right-left+1
             right+=1
-        #Return ans
-        return ans
-
+        #Return count
+        return count
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         #In a single function counting is very hard when we try to shrink window from left.
         #So (Subarrays<=k odd nos)-(<=k-1 odd nos)=(Subarrays==k odd nos)
-        return self.func(nums,k)-self.func(nums,k-1)
+        return self.func(nums, k)-self.func(nums, k-1)
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
