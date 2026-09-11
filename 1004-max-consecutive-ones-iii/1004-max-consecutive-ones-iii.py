7fe02c9196f8b92461f0f1f4c0bb42960c02882d
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-11 11:31:46 +0530

    Time: 75 ms (13.4%), Space: 22.1 MB (85.69%) - LeetHub - Time: O(n), Space: O(1)

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -2,15 +2,14 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         #Sliding Window
         n=len(nums)
-        left, right=0, 0
-        maxi=0
+        left=right=maxi=0
         while right<n :
             #If nums[right]==1 then increment right
             if nums[right]==1 : right+=1
             #If nums[right]==0 and k then decrement k, increment right
-            elif nums[right]==0 and k :  
-                k-=1
+            elif nums[right]==0 and k : 
                 right+=1
+                k-=1
             #We have to shrink the window by incrementing left
             else :
                 #if nums[left]==0 then increment k
@@ -20,7 +19,7 @@ class Solution:
             maxi=max(maxi, right-left)
         #Return maxi
         return maxi
-                
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
