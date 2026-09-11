0edace37c5ac1b7768249a624169a36edd928ad8
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-11 11:29:25 +0530

    Time: 215 ms (34.85%), Space: 19.9 MB (53.85%) - LeetHub - Time: O(n), Space: O(min(n, m))

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -2,9 +2,8 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         #Sliding Window
         n=len(s)
-        st=set()            #Declare set()
-        maxi=0
-        left, right=0, 0
+        left=right=maxi=0
+        st=set()        #Declare set()
         for right in range(n) :
             #If s[right] is already there in set then we have to shrink from left side for the purpose of removing it
             while s[right] in st :
@@ -15,7 +14,6 @@ class Solution:
             maxi=max(maxi, right-left+1)
         #Return maxi
         return maxi
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
