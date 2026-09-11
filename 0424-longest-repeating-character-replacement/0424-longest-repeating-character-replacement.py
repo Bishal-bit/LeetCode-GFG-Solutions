20dca3dd061a9bac229417e7aae04d530399cec7
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-11 14:17:18 +0530

    Time: 79 ms (75.22%), Space: 19.6 MB (52.27%) - LeetHub - Time: O(n), Space: O(1)

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,26 +2,23 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         n=len(s)
         mp={}
+        left=right=0
         #freq is for storing the character having highest frequency
-        freq, ans=0, 0
-        left, right=0, 0
-        while right<n :
+        freq=maxi=0
+        while right< n :
             #Update mp[s[right]] based on frequency 
             mp[s[right]]=mp.get(s[right],0)+1
             #Update freq
-            freq=max(freq, mp[s[right]])
+            freq=max(freq,mp[s[right]])
             #If (right-left+1)-freq exceeds k then decrement mp[s[left]],increment left
-            if (right-left+1)-freq>k :
+            if (right-left+1)-freq > k :
                 mp[s[left]]-=1
                 left+=1
-            #Update ans, increment right
-            ans=max(ans, right-left+1)
+            #Update maxi, increment right
+            maxi=max(maxi, right-left+1)
             right+=1
-        #Return ans
-        return ans
-            
-
-
+        #Return maxi
+        return maxi
 
 
 # Synced seamlessly with LeetHub Pro
