cba6aac73d3c0e74072430234772e06f92ad548b
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-12 10:40:08 +0530

    Time: 133 ms (62.58%), Space: 21.9 MB (93.05%) - LeetHub - Time: O(n), Space: O(n)

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,29 +1,32 @@
 class Solution:
-    def func(self, nums: List[int], k: int ) :
+    def func(self, nums: List[int], k: int) :
         #Sliding Window
         n=len(nums)
         #Declare a map mp to record frequency of integers
         mp={}
-        left, ans=0, 0
-        for right in range(n) :
+        left=right=ans=0
+        while right< n :
             #If it is a new integer i.e. not present in map mp then decrement k
             if nums[right] not in mp : k-=1
             mp[nums[right]]=mp.get(nums[right],0)+1
+            
             #While k<0 : shrink window from left side
             while k<0 :
                 mp[nums[left]]-=1
-                if mp[nums[left]]==0 : 
-                    mp.pop(nums[left])
+                if mp[nums[left]]==0 :
                     k+=1
+                    mp.pop(nums[left])
                 left+=1
+            
             #Update ans
             ans+=right-left+1
+            right+=1
         return ans
 
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         #In a single function counting is very hard when we try to shrink window from left.
         #So (Subarrays <=k different nos)-(<=k-1 different nos)=(Subarrays==k different nos)
-        return self.func(nums, k) - self.func(nums, k-1)
+        return self.func(nums, k)-self.func(nums, k-1)
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
