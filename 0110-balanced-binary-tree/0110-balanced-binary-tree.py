bc2fab5a34bcbb42e821ff77cd5108f1c4dc1a04
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-12 14:35:56 +0530

    Time: 3 ms (50.09%), Space: 20.4 MB (39.04%) - LeetHub - Time: O(N), Space: O(H)

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,20 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def func(self, root: Optional[TreeNode])->int :
-        if not root : return 0
-        l=self.func(root.left)
-        r=self.func(root.right)
+    def func(self, node: Optional[TreeNode])->int :
+        if not node : return 0
+        l=self.func(node.left)
+        r=self.func(node.right)
         #Check for imbalance
         if l==-1 or r==-1 or abs(l-r)>1 : return -1
-        return 1+max(l,r)
-
+        return 1+ max(l, r)
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        #Use max depth of Binary tree algorithm
+        #Use max depth algorithm of Binary tree 
         #If it is imbalanced then function returns -1
         return self.func(root)!=-1
-
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
