9b30fecb8d57ef42159a82cbce23bbad9fd7c611
Bishal Sarkar <72163921+Bishal-bit@users.noreply.github.com>
2026-09-12 14:35:58 +0530

    Time: 4 ms (44.89%), Space: 22.1 MB (88.93%) - LeetHub - Time: O(N), Space: O(H)

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,18 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def func(self, root: Optional[TreeNode]) ->int:
-        if root is None : return 0
-        l=self.func(root.left)
-        r=self.func(root.right)
+    def func(self, node: Optional[TreeNode])->int :
+        if not node : return 0
+        l=self.func(node.left)
+        r=self.func(node.right)
         #Update self.diam
         self.diam=max(self.diam, l+r)
+        #Return 1+max(l,r)
         return 1+max(l,r)
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         #self.diam is similer to pass by reference 
         self.diam=0
         self.func(root)
+        #Return self.diam
         return self.diam
 
 # Synced seamlessly with LeetHub Pro
